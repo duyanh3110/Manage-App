fbf9d261143f468f000ad195a3fbb9da157bb29f
Nguyen Duy Anh <duyanh19962012@gmail.com>
2018-11-08 14:04:34 +0200

    Add filter name and status in TaskList component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ class App extends Component {
     this.state = {
       tasks: [],
       isDisplayForm: false,
-      taskEditing: null
+      taskEditing: null,
+      filter: {
+        name: '',
+        status: -1
+      }
     }
   }
 
@@ -120,8 +124,32 @@ class App extends Component {
     return result;
   };
 
+  onFilter = (filterName, filterStatus) => {
+    filterStatus = parseInt(filterStatus, 10);
+    this.setState({
+      filter: {
+        name: filterName.toLowerCase(),
+        status: filterStatus
+      }
+    });
+  };
+
   render() {
-    let { tasks, isDisplayForm, taskEditing } = this.state;
+    let { tasks, isDisplayForm, taskEditing, filter } = this.state;
+    if(filter) {
+      if(filter.name) {
+        tasks = tasks.filter((task) => {
+          return task.name.toLowerCase().indexOf(filter.name) !== -1;
+        });
+      }
+      tasks = tasks.filter((task) => {
+        if (filter.status === -1) {
+          return task;
+        } else {
+          return task.status === ( filter.status === 1 ? true : false);
+        }
+      })
+    }
     var elmTaskForm = isDisplayForm
           ? <TaskForm
               onCloseForm={ this.onCloseForm }
@@ -154,6 +182,7 @@ class App extends Component {
               onUpdateStatus={ this.onUpdateStatus }
               onDelete={ this.onDelete }
               onUpdate={ this.onUpdate }
+              onFilter={ this.onFilter }
             />
           </div>
         </div>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import TaskItem from './TaskItem';
 
 class TaskList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterName: '',
+      filterStatus: -1 // -1: all, 1: active, 0: disable
+    };
+  };
+
+  onChange = (event) => {
+    let target = event.target;
+    let name = target.name;
+    let value = target.value;
+    this.props.onFilter(
+      name === 'filterName' ? value : this.state.filterName,
+      name === 'filterStatus' ? value : this.state.filterStatus
+    );
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     let { tasks } = this.props;
+    let { filterName, filterStatus } = this.state;
     let elmTasks = tasks.map((task, index) => {
       return <TaskItem
         key={ task.id }
@@ -34,12 +56,16 @@ class TaskList extends Component {
                   type="text"
                   className="form-control"
                   name="filterName"
+                  value={ filterName }
+                  onChange={ this.onChange }
                 />
               </td>
               <td>
                 <select
                   name="filterStatus"
                   className="form-control"
+                  value={ filterStatus }
+                  onChange={ this.onChange }
                 >
                   <option value={-1}>All</option>
                   <option value={0}>Disable</option>
